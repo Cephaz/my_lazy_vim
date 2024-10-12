c2ce311b2fb8037ec627e74175c80913231dad31
Pierre ZHOU <pierre@highcast.co>
2024-10-12 21:37:55 +0200

    update

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -4,7 +4,10 @@ return {
     opts = {
       filesystem = {
         filtered_items = {
-          visible = true,
+          hide_dotfiles = false,
+          visible = false, -- when true, they will just be displayed differently than normal items
+          force_visible_in_empty_folder = true, -- when true, hidden files will be shown if the root folder is otherwise empty
+          show_hidden_count = false, -- when true, the number of hidden items in each folder will be shown as the last entry
         },
       },
     },
