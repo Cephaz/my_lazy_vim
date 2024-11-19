adfdf597ff0528610990d6053d5669c0f2781731
Pierre ZHOU <pierre@highcast.co>
2024-11-19 16:34:24 +0100

    prettier - cancel autosave

--- lua/plugins/autosave.lua
+++ lua/plugins/autosave.lua
@@ -1,3 +1,7 @@
+if true then
+  return {}
+end
+
 return {
   {
     "Pocco81/auto-save.nvim",
--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,14 +1,18 @@
 return {
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    opts = {
-      filesystem = {
-        filtered_items = {
-          hide_dotfiles = false,
-          visible = false, -- when true, they will just be displayed differently than normal items
-          force_visible_in_empty_folder = true, -- when true, hidden files will be shown if the root folder is otherwise empty
-          show_hidden_count = false, -- when true, the number of hidden items in each folder will be shown as the last entry
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = {
+    filesystem = {
+      filtered_items = {
+        visible = true,
+        show_hidden_count = true,
+        hide_dotfiles = false,
+        hide_gitignored = true,
+        hide_by_name = {
+          -- '.git',
+          -- '.DS_Store',
+          -- 'thumbs.db',
         },
+        never_show = {},
       },
     },
   },
