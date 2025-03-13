a85aa8a55ec5772fef2936362e54adf9095bdbcd
Pierre ZHOU <pierre@highcast.co>
2025-03-13 11:21:08 +0100

    update: theme | lsp ts | tf

--- lua/plugins/lsp_ts.lua
+++ lua/plugins/lsp_ts.lua
@@ -29,21 +29,21 @@ return {
       },
     }
 
-    require("mason").setup()
-
-    local ensure_installed = vim.tbl_keys(servers)
-    require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
-
-    require("mason-lspconfig").setup({
-      ensure_installed = ensure_installed,
-      handlers = {
-        function(server_name)
-          local opts = servers[server_name] or {}
-          opts.capabilities = vim.tbl_deep_extend("force", {}, capabilities, opts.capabilities or {})
-          require("lspconfig")[server_name].setup(opts)
-        end,
-      },
-    })
+    -- require("mason").setup()
+    --
+    -- local ensure_installed = vim.tbl_keys(servers)
+    -- require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
+    --
+    -- require("mason-lspconfig").setup({
+    --   ensure_installed = ensure_installed,
+    --   handlers = {
+    --     function(server_name)
+    --       local opts = servers[server_name] or {}
+    --       opts.capabilities = vim.tbl_deep_extend("force", {}, capabilities, opts.capabilities or {})
+    --       require("lspconfig")[server_name].setup(opts)
+    --     end,
+    --   },
+    -- })
 
     vim.api.nvim_create_autocmd("LspAttach", {
       group = vim.api.nvim_create_augroup("kickstart-lsp-attach", { clear = true }),
@@ -58,7 +58,6 @@ return {
         map("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
         map("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
         map("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
-        map("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
         map("<leader>cr", vim.lsp.buf.rename, "[R]e[n]ame")
         map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction", { "n", "x" })
         map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
