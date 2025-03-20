37642d43a521ae8752224e66b5a698275f3abbbb
Cephaz <pierrezhoou@gmail.com>
2025-03-20 20:56:37 +0100

    update: gruvbox theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,33 +1,15 @@
 return {
-  {
-    "rebelot/kanagawa.nvim",
-    priority = 1000, -- S'assurer que le thème est chargé en priorité
-    config = function()
-      require("kanagawa").setup({
-        compile = false, -- Activer la compilation du thème pour de meilleures performances
-        undercurl = true, -- Activer les soulignements en courbe
-        commentStyle = { italic = true },
-        functionStyle = {},
-        keywordStyle = { italic = true },
-        statementStyle = { bold = true },
-        typeStyle = {},
-        transparent = false, -- Ne pas rendre le fond transparent
-        dimInactive = false, -- Ne pas assombrir les fenêtres inactives
-        terminalColors = true, -- Appliquer les couleurs du thème au terminal intégré
-        colors = { -- Personnalisation des couleurs (laisser vide si pas besoin)
-          palette = {},
-          theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
-        },
-        overrides = function(colors) -- Personnalisation des highlights (laisser vide si pas besoin)
-          return {}
-        end,
-        theme = "dragon", -- Charger la version "Dragon" du thème
-        background = {
-          dark = "dragon",
-          light = "lotus",
-        },
-      })
-      vim.cmd("colorscheme kanagawa-dragon") -- Appliquer le thème après la configuration
-    end,
-  },
+  "sainnhe/gruvbox-material",
+  priority = 1000, -- S'assure que le thème est chargé en premier
+  config = function()
+    -- Options spécifiques à Gruvbox Material
+    vim.g.gruvbox_material_background = "hard" -- Options: 'soft', 'medium', 'hard'
+    vim.g.gruvbox_material_foreground = "material"
+    vim.g.gruvbox_material_enable_bold = 1
+    vim.g.gruvbox_material_enable_italic = 1
+    vim.g.gruvbox_material_transparent_background = 0 -- Mettre à 1 pour un fond transparent
+
+    -- Appliquer le thème
+    vim.cmd.colorscheme("gruvbox-material")
+  end,
 }
