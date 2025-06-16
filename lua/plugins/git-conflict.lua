a172e719ae2f9ad22f888aab8158d417094cd2d3
Pierre ZHOU <pierre@highcast.co>
2025-06-16 09:16:31 +0200

    feat: git-conflict

--- lua/plugins/git-conflict.lua
+++ lua/plugins/git-conflict.lua
@@ -3,12 +3,16 @@ return {
   version = "*",
   config = function()
     require("git-conflict").setup({
-      default_mappings = true, -- active les mappings par défaut
-      disable_diagnostics = true, -- évite les messages d'erreur pendant le conflit
+      default_mappings = false, -- on les définit nous-mêmes
+      disable_diagnostics = true,
       highlights = {
         incoming = "DiffAdd",
         current = "DiffText",
       },
     })
+
+    vim.keymap.set("n", "[x", "<Plug>(git-conflict-ours)", { desc = "Git Conflict - Choose current" })
+    vim.keymap.set("n", "]x", "<Plug>(git-conflict-theirs)", { desc = "Git Conflict - Choose incoming" })
+    vim.keymap.set("n", "cx", "<Plug>(git-conflict-both)", { desc = "Git Conflict - Choose both" })
   end,
 }
